a4afc1ae7790d0863931ddc3b5203edc77c50d7f
Martynas Skaringa <skarware@gmail.com>
2020-08-01 19:57:07 +0300

    Initial commit

---
+++ src/main/java/edu/technical/task/cdrapi/CdrapiApplication.java
@@ -0,0 +1,13 @@
+package edu.technical.task.cdrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdrapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdrapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/technical/task/cdrapi/CdrapiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.technical.task.cdrapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdrapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
