d85bce761a9102e7769d0a7e2d727e4f0ebc5de8
Martynas Skaringa <skarware@gmail.com>
2020-08-01 21:18:35 +0300

    Add SQL schema startup script with some test data, change DDL-auto into validation mode

---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,19 @@
+create table USER_ACCOUNTS
+(
+    PHONE_NUMBER VARCHAR(255) not null primary key
+);
+
+create table CALL_DETAIL_RECORDS
+(
+    ID              UUID                     not null primary key,
+    COST_PER_MINUTE DOUBLE,
+    END_DATE        TIMESTAMP not null,
+    START_DATE      TIMESTAMP not null,
+    STATUS          BOOLEAN,
+    ACCOUNT         VARCHAR(255)             not null,
+    DESTINATION     VARCHAR(255)             not null,
+    constraint FKHM6UM5Y8NXYLF8T8U1E5XT6FF
+        foreign key (DESTINATION) references USER_ACCOUNTS (PHONE_NUMBER),
+    constraint FKK692W4696EFC829TLSR72LRQ9
+        foreign key (ACCOUNT) references USER_ACCOUNTS (PHONE_NUMBER)
+);
